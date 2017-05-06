97f18a5ffa83c891c22b17ce213c64e95f21e384
Steve Frost <ffrostt@gmail.com>
2017-05-05 23:13:17 -0400

    completed pangram challenge w/ regex lookaheads

--- ecmascript/leap/leap.js
+++ ecmascript/leap/leap.js
@@ -14,6 +14,3 @@ class Leap {
 }
 
 export default Leap
-
-
-
---
+++ javascript/pangram/pangram.js
@@ -0,0 +1,9 @@
+function Pangram(sentence) {
+  this.sentence = sentence;
+}
+
+Pangram.prototype.isPangram = function isPangram() {
+  return /(?=.*a)(?=.*b)(?=.*c)(?=.*d)(?=.*e)(?=.*f)(?=.*g)(?=.*h)(?=.*i)(?=.*j)(?=.*k)(?=.*l)(?=.*m)(?=.*n)(?=.*o)(?=.*p)(?=.*q)(?=.*r)(?=.*s)(?=.*t)(?=.*u)(?=.*v)(?=.*w)(?=.*x)(?=.*y)(?=.*z)./i.test(this.sentence)
+};
+
+module.exports = Pangram;
